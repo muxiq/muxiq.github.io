f36d9bbcb13e946d0587298810ed43f99cdc3f3b
muxiq <herff8@gmail.com>
2025-04-20 20:58:53 -0700

    Add files via upload

---
+++ perfis.js
@@ -0,0 +1,13 @@
+
+const perfis = [
+  {
+    username: "nezay",
+    nome: "Nezay",
+    avatar: "https://i.imgur.com/3GvwNBf.jpeg",
+    bio: "fazendo códigos e caos",
+    links: [
+      { nome: "Discord", url: "https://discord.com/users/nz4" },
+      { nome: "GitHub", url: "https://github.com/nz4" }
+    ]
+  }
+];
